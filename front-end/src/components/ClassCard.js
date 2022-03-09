cc7a0ce5f690c1117c4bdf35bb67e587049a9eca
Verse <verseow@gmail.com>
2022-03-09 12:17:23 -0500

    tailwind and class card

---
+++ front-end/src/components/ClassCard.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { useRouter } from 'next/router';
+
+function ClassCard(props) {
+  const router = useRouter();
+
+  return (
+    // when div is clicked redirect to the class page with the class title as the url
+    <div
+      className="fixed rounded-xl bg-blue-200 p-4 transition-all ease-linear hover:rounded-md hover:bg-blue-400"
+      onClick={() => {
+        router.push(`/class/${props.title}`);
+      }}>
+      <image src={props.image} />
+      <h1>{props.title}</h1>
+      <p>{props.description}</p>
+      <p>Enrolled: {props.enrolled}</p>
+    </div>
+  );
+}
+
+ClassCard.propTypes = {};
+
+export default ClassCard;
