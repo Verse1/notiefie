941da56cfff20f38fccefecd2b58373447bfe903
Verse <verseow@gmail.com>
2022-03-09 14:06:15 -0500

    navbar fix and i dont know kinda login

---
+++ front-end/src/pages/login.jsx
@@ -0,0 +1,15 @@
+function login() {
+    return <div>
+        <h1>Login</h1>
+        <form>
+            <label>
+                School Email:
+                <input type="text" name="email" />
+            </label>
+            
+        </form>
+        
+  </div>;
+}
+
+export default login;
