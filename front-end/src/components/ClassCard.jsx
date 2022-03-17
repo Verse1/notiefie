3757d022e4e696a8482aa6735b03d8fdd947993e
Verse <verseow@gmail.com>
2022-03-17 18:11:27 -0400

    reviewed code fixed redirects and navbar

--- front-end/src/components/ClassCard.jsx
+++ front-end/src/components/ClassCard.jsx
@@ -10,28 +10,27 @@ function ClassCard(props) {
 
   const handlePin = () => {
     setPin(!pin);
-
   };
 
   return (
     <>
-    <div className={`cursor-pointer rounded-3xl bg-sky-500 p-7 font-sans transition-all linear hover:rounded-xl hover:bg-sky-400 relative my-3 text-x order-${props.order}`}>
+      <div
+        className={`linear text-x relative my-3 cursor-pointer rounded-3xl bg-sky-500 p-7 font-sans transition-all hover:rounded-xl hover:bg-sky-400 order-${props.order}`}>
+        <button>
+          {pin ? (
+            <AiFillPushpin onClick={() => handlePin()} />
+          ) : (
+            <AiOutlinePushpin
+              onClick={() => handlePin()}
+              className="hover:fill-blue-500"
+            />
+          )}
+        </button>
 
-      <button >
-        {pin ? (
-          <AiFillPushpin onClick={() => handlePin()}/>
-        ) : (
-          <AiOutlinePushpin
-            onClick={() => handlePin()}
-            className="hover:fill-blue-500"
-          />
-        )}
-      </button>
-
-      <image src={props.image} />
-      <h1>{props.title}</h1>
-      <p>{props.description}</p>
-      <p>Enrolled: {props.enrolled}</p>
+        <image src={props.image} />
+        <h1>{props.title}</h1>
+        <p>{props.description}</p>
+        <p>Enrolled: {props.enrolled}</p>
       </div>
     </>
   );
--- front-end/src/pages/index.js
+++ front-end/src/pages/index.js
@@ -5,27 +5,12 @@ import ClassCard from '@/components/ClassCard';
 export default function Home() {
   return (
     <div className="grid place-items-center">
-
       <ClassCard
         title="Intro to CO"
         description="CS is just the description"
         enrolled="600"
         order="last"
       />
-
-      <ClassCard
-        title="Intro to CS"
-        description="CS is just the description"
-        enrolled="700"
-        order="0"
-      />
-
-      {/* <ClassCard
-        title="Intro to CS"
-        description="CS is just the description"
-        enrolled="600"
-        order="0"
-      /> */}
     </div>
   );
 }
