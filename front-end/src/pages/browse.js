7e584dfd72a835cb9ea72c901bfac047387fbd8e
Verse <verseow@gmail.com>
2022-03-19 17:12:07 -0400

    Revert "added class selector component, packages clsx and @heroicons/react, changed browse link to classes"
    This reverts commit cbbf7ea88a2ca845271ef25312bea0eabde14338.

---
+++ front-end/src/pages/browse.js
@@ -0,0 +1,130 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { useRouter } from 'next/router';
+import BrowseClassCard from '@/components/BrowseClassCard';
+import Navigation from '@/components/Navigation';
+import Head from 'next/head';
+
+function BrowseClasses(props) {
+  const router = useRouter();
+
+  const classes = [
+    {
+      classCode: 'CSCI-UA.0101',
+      name: 'Introduction to Computer Science',
+      color: 'bg-teal-400',
+      hover: 'hover:bg-teal-300',
+    },
+    {
+      classCode: 'CSCI-UA.0201',
+      name: 'Computer Systems Organization',
+      color: 'bg-cyan-400',
+      hover: 'hover:bg-cyan-300',
+    },
+    {
+      classCode: 'CSCI-UA.0465',
+      name: 'Introduction to Robotics',
+      color: 'bg-indigo-400',
+      hover: 'hover:bg-indigo-300',
+    },
+    {
+      classCode: 'CSCI-UA.0478',
+      name: 'Introduction to Cryptography',
+      color: 'bg-pink-400',
+      hover: 'hover:bg-pink-300',
+    },
+    {
+      classCode: 'CSCI-UA.0102',
+      name: 'Data Structures',
+      color: 'bg-cyan-400',
+      hover: 'hover:bg-cyan-300',
+    },
+    {
+      classCode: 'CSCI-UA.0421',
+      name: 'Numerical Computing',
+      color: 'bg-pink-400',
+      hover: 'hover:bg-pink-300',
+    },
+    {
+      classCode: 'CSCI-UA.0436',
+      name: 'Computer Architecture',
+      color: 'bg-teal-400',
+      hover: 'hover:bg-teal-300',
+    },
+    {
+      classCode: 'CSCI-UA.0467',
+      name: 'Applied Internet Technology',
+      color: 'bg-indigo-400',
+      hover: 'hover:bg-indigo-300',
+    },
+    {
+      classCode: 'CSCI-UA.0101',
+      name: 'Introduction to Computer Science',
+      color: 'bg-teal-400',
+      hover: 'hover:bg-teal-300',
+    },
+    {
+      classCode: 'CSCI-UA.0201',
+      name: 'Computer Systems Organization',
+      color: 'bg-cyan-400',
+      hover: 'hover:bg-cyan-300',
+    },
+    {
+      classCode: 'CSCI-UA.0465',
+      name: 'Introduction to Robotics',
+      color: 'bg-indigo-400',
+      hover: 'hover:bg-indigo-300',
+    },
+    {
+      classCode: 'CSCI-UA.0478',
+      name: 'Introduction to Cryptography',
+      color: 'bg-pink-400',
+      hover: 'hover:bg-pink-300',
+    },
+    {
+      classCode: 'CSCI-UA.0102',
+      name: 'Data Structures',
+      color: 'bg-cyan-400',
+      hover: 'hover:bg-cyan-300',
+    },
+    {
+      classCode: 'CSCI-UA.0421',
+      name: 'Numerical Computing',
+      color: 'bg-pink-400',
+      hover: 'hover:bg-pink-300',
+    },
+    {
+      classCode: 'CSCI-UA.0436',
+      name: 'Computer Architecture',
+      color: 'bg-teal-400',
+      hover: 'hover:bg-teal-300',
+    },
+    {
+      classCode: 'CSCI-UA.0467',
+      name: 'Applied Internet Technology',
+      color: 'bg-indigo-400',
+      hover: 'hover:bg-indigo-300',
+    },
+  ];
+
+  return (
+    <div>
+      <div className="mx-auto mt-20 max-w-screen-lg p-4">
+        <div className="mt-12 grid h-1/3 grid-cols-[repeat(4,_minmax(200px,_1fr))] gap-4">
+          {classes.map((c, i) => (
+            <BrowseClassCard
+              key={i.toString()}
+              classCode={c.classCode}
+              name={c.name}
+              color={c.color}
+              hover={c.hover}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+BrowseClasses.propTypes = {};
+export default BrowseClasses;
--- front-end/src/pages/login.js
+++ front-end/src/pages/login.js
@@ -1,33 +1,27 @@
-const Login = () =>{
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log({email: e.target.email.value});
-  };
-
+function login() {
   return (
     <div className="grid h-screen place-items-center">
       <div className="bg inline-block rounded-3xl bg-white p-7">
         <h1 className="mb-7 text-center text-2xl">Login to your account</h1>
-        <form className="text-center" onSubmit={handleSubmit}>
-          <label htmlFor="login-email">
+        <form className="text-center">
+          <label>
             School Email:
             <input
-              type="email"
-              id="login-email"
+              type="text"
               name="email"
-              className="ml-2 p-1 px-2 rounded-md border border-black"
-              required
+              className="ml-2 rounded-md border border-black"
             />
           </label>
           <br />
-          <button
+          <input
             type="submit"
+            value="Sign In"
             className="mt-9 mb-4 cursor-pointer rounded-xl bg-gradient-to-r from-green-300 to-blue-400 p-3 px-9"
-          >Sign In</button>
+          />
         </form>
       </div>
     </div>
   );
 }
 
-export default Login;
+export default login;
