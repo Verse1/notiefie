840aaf4212a308ca1ec55c66068ca61801008497
isabelleoktay <60681704+isabelleoktay@users.noreply.github.com>
2022-03-24 13:12:08 -0400

    padding fixed 3 ksfhdksv

--- front-end/src/components/Attachment.jsx
+++ front-end/src/components/Attachment.jsx
@@ -7,7 +7,7 @@ const Attachment = props => {
     const router = useRouter();
 
     return (
-        <div className='w-20 h-20 p-1.5g bg-slate-100 inline-block mx-10 rounded-2xl'>
+        <div className='w-20 h-20 p-1.5 bg-slate-100 inline-block mx-10 rounded-2xl'>
           <AiOutlineDownload size={22} className="hover:fill-emerald-400 fill-slate-800 float-right"/>
         </div>
     )
