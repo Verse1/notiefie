a24ecf9e4e16a9c98b40823a71147cecdeb2899c
Verse <verseow@gmail.com>
2022-03-25 19:07:35 -0400

    settings page init

---
+++ front-end/src/pages/settings.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function settings() {
+
+  return (
+    <div className="grid place-items-center">
+      <div className={`relative h-auto w-[28%] rounded-3xl bg-sky-500 text-lg`}>
+        
+
+        <div className="p-5 text-center text-white">
+          <h1 className="text-left ">Foo</h1>
+          <p className="flex text-right">New York University</p>
+
+          <p>204 Upvotes</p>
+          <p>Joined February 27,2022</p>
+              </div>
+          </div>
+          </div>
+
+  )
+}
