2a66308a19bd642552a15804d1ac704b975a1885
Verse <verseow@gmail.com>
2022-03-25 20:17:18 -0400

    added classes to next api

--- front-end/src/pages/browse.js
+++ front-end/src/pages/browse.js
@@ -1,123 +1,25 @@
-import React from 'react';
+import { React, useState } from 'react';
 import PropTypes from 'prop-types';
 import { useRouter } from 'next/router';
 import BrowseClassCard from '../components/BrowseClassCard';
 import Navigation from '../components/Navigation';
 import Head from 'next/head';
+import axios from 'axios';
 
-function BrowseClasses(props) {
-  const router = useRouter();
-
-  const classes = [
-    {
-      classCode: 'CSCI-UA.0101',
-      name: 'Introduction to Computer Science',
-      color: 'bg-teal-400',
-      hover: 'hover:bg-teal-300',
-    },
-    {
-      classCode: 'CSCI-UA.0201',
-      name: 'Computer Systems Organization',
-      color: 'bg-cyan-400',
-      hover: 'hover:bg-cyan-300',
-    },
-    {
-      classCode: 'CSCI-UA.0465',
-      name: 'Introduction to Robotics',
-      color: 'bg-indigo-400',
-      hover: 'hover:bg-indigo-300',
-    },
-    {
-      classCode: 'CSCI-UA.0478',
-      name: 'Introduction to Cryptography',
-      color: 'bg-pink-400',
-      hover: 'hover:bg-pink-300',
-    },
-    {
-      classCode: 'CSCI-UA.0102',
-      name: 'Data Structures',
-      color: 'bg-cyan-400',
-      hover: 'hover:bg-cyan-300',
-    },
-    {
-      classCode: 'CSCI-UA.0421',
-      name: 'Numerical Computing',
-      color: 'bg-pink-400',
-      hover: 'hover:bg-pink-300',
-    },
-    {
-      classCode: 'CSCI-UA.0436',
-      name: 'Computer Architecture',
-      color: 'bg-teal-400',
-      hover: 'hover:bg-teal-300',
-    },
-    {
-      classCode: 'CSCI-UA.0467',
-      name: 'Applied Internet Technology',
-      color: 'bg-indigo-400',
-      hover: 'hover:bg-indigo-300',
-    },
-    {
-      classCode: 'CSCI-UA.0101',
-      name: 'Introduction to Computer Science',
-      color: 'bg-teal-400',
-      hover: 'hover:bg-teal-300',
-    },
-    {
-      classCode: 'CSCI-UA.0201',
-      name: 'Computer Systems Organization',
-      color: 'bg-cyan-400',
-      hover: 'hover:bg-cyan-300',
-    },
-    {
-      classCode: 'CSCI-UA.0465',
-      name: 'Introduction to Robotics',
-      color: 'bg-indigo-400',
-      hover: 'hover:bg-indigo-300',
-    },
-    {
-      classCode: 'CSCI-UA.0478',
-      name: 'Introduction to Cryptography',
-      color: 'bg-pink-400',
-      hover: 'hover:bg-pink-300',
-    },
-    {
-      classCode: 'CSCI-UA.0102',
-      name: 'Data Structures',
-      color: 'bg-cyan-400',
-      hover: 'hover:bg-cyan-300',
-    },
-    {
-      classCode: 'CSCI-UA.0421',
-      name: 'Numerical Computing',
-      color: 'bg-pink-400',
-      hover: 'hover:bg-pink-300',
-    },
-    {
-      classCode: 'CSCI-UA.0436',
-      name: 'Computer Architecture',
-      color: 'bg-teal-400',
-      hover: 'hover:bg-teal-300',
-    },
-    {
-      classCode: 'CSCI-UA.0467',
-      name: 'Applied Internet Technology',
-      color: 'bg-indigo-400',
-      hover: 'hover:bg-indigo-300',
-    },
-  ];
-
+function BrowseClasses({ classes }) {
   return (
     <div>
+      {console.log(classes)}
+
       <div className="mx-auto mt-20 max-w-screen-lg p-4">
         <div className="mt-12 grid h-1/3 grid-cols-[repeat(4,_minmax(200px,_1fr))] gap-4">
-          {classes.map((c, i) => (
+          {classes.map((classCard) => (
             <BrowseClassCard
-              key={i.toString()}
-              classCode={c.classCode}
-              name={c.name}
-              color={c.color}
-              hover={c.hover}
+              key={classCard.classCode}
+              classCode={classCard.classCode}
+              name={classCard.name}
+              color={classCard.color}
+              hover={classCard.hover}
             />
           ))}
         </div>
@@ -125,6 +27,12 @@ function BrowseClasses(props) {
     </div>
   );
 }
+
+export const getStaticProps = async () => {
+  const res = await axios.get(process.env.API_URL + '/classes');
+  const classes = await res.data;
+  return { props: { classes } };
+};
 
 BrowseClasses.propTypes = {};
 export default BrowseClasses;
