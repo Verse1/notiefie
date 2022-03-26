0f98121e6825d55ceb9b44034c6a5255a80cedd8
Verse <verseow@gmail.com>
2022-03-25 21:11:40 -0400

    added notes api

--- front-end/src/pages/class.js
+++ front-end/src/pages/class.js
@@ -2,17 +2,28 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { useRouter } from 'next/router';
 import ClassHeader from '../components/ClassHeader';
+import axios from 'axios';
 
-const IndividualClass = props => {
-    const router = useRouter();
+const IndividualClass = ({ notes }) => {
+  const router = useRouter();
 
-    return (
-        <div>
-            <ClassHeader color="bg-sky-500" name="Intro to Computer Science" code="CS101" posts="10" enrolled="60"/>
-        </div>
-    )
-
-}
-
+  return (
+    <div>
+      <ClassHeader
+        color="bg-sky-500"
+        name="Intro to Computer Science"
+        code="CS101"
+        posts="10"
+        enrolled="60"
+        notes={notes}
+      />
+    </div>
+  );
+};
+export const getStaticProps = async () => {
+  const res = await axios.get(process.env.API_URL + '/notes');
+  const notes = await res.data;
+  return { props: { notes } };
+};
 IndividualClass.propTypes = {};
 export default IndividualClass;
