8650d6f01a1fafd2cb192533f262363b117d774a
Verse <verseow@gmail.com>
2022-03-26 17:54:20 -0400

    repositioned notifications popup

--- front-end/src/components/NotificationsBell.jsx
+++ front-end/src/components/NotificationsBell.jsx
@@ -46,7 +46,7 @@ export default function NavigationBell() {
             leave="transition ease-in duration-150"
             leaveFrom="opacity-100 translate-y-0"
             leaveTo="opacity-0 translate-y-1">
-            <Popover.Panel className="fixed left-1/2 z-10 ml-[41rem] mt-3 max-w-sm -translate-x-1/2 transform px-4 sm:px-0 lg:max-w-3xl">
+            <Popover.Panel className="fixed left-1/2 z-10 ml-[61rem] mt-3 max-w-sm -translate-x-1/2 transform px-4 sm:px-0 lg:max-w-3xl">
               <div className="overflow-hidden rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5 ">
                 <div className="relative grid gap-8 p-7 lg:grid-cols-1">
                   {notifications.map((item) => (
