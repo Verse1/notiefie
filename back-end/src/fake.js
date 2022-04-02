60a62fc86f8583c32db7fcbe8f697b9b23a7fcdc
isabelleoktay <60681704+isabelleoktay@users.noreply.github.com>
2022-04-02 14:01:22 -0400

    started browse for real

--- back-end/src/fake.js
+++ back-end/src/fake.js
@@ -1,29 +1,53 @@
 const faker = require('faker');
 
 function users(n) {
-    let users = [];
-    
-    for (let i = 1; i <= n; i++){
-        users.push({
-            id: faker.datatype.uuid(),
-            name: faker.name.findName(),
-            university: faker.address.cityName()+' University',
-            email: faker.internet.email(),
-            password: faker.internet.password(),
-            createdAt: faker.date.past(),
-        });
-    }
+  let users = [];
+
+  for (let i = 1; i <= n; i++) {
     users.push({
-        id: "2005b873-dd55-4ebe-8165-76ce6d9b83a6",
-        name: "Test",
-        university: "Test University",
-        email: "test@test.com",
-        password: "test",
-        createdAt: "2019-01-01T00:00:00.000Z",
+      id: faker.datatype.uuid(),
+      name: faker.name.findName(),
+      university: faker.address.cityName() + ' University',
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+      createdAt: faker.date.past(),
     });
-    return users;
+  }
+  users.push({
+    id: '2005b873-dd55-4ebe-8165-76ce6d9b83a6',
+    name: 'Test',
+    university: 'Test University',
+    email: 'test@test.com',
+    password: 'test',
+    createdAt: '2019-01-01T00:00:00.000Z',
+  });
+  return users;
 }
 
 module.exports = {
-    users
+  users,
 };
+
+function classes(n) {
+  const classes = [];
+
+  for (let i = 1; i <= n; i++) {
+    classes.push({
+      id: faker.datatype.uuid(),
+      name: faker.lorem.words() + '101',
+      classCode: faker.random.word() + '-101',
+      university: faker.address.cityName() + ' University',
+      password: faker.internet.password(),
+      createdAt: faker.date.past(),
+    });
+  }
+  classes.push({
+    id: '2005b873-dd55-4ebe-8165-76ce6d9b83a6',
+    name: 'Test',
+    university: 'Test University',
+    email: 'test@test.com',
+    password: 'test',
+    createdAt: '2019-01-01T00:00:00.000Z',
+  });
+  return classes;
+}
