66c5ff36d4ac0351758cc61edd74af45526e5e68
isabelleoktay <60681704+isabelleoktay@users.noreply.github.com>
2022-04-02 15:14:32 -0400

    deleted classes folder

--- back-end/src/routes/classes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const allClasses = require('./test/classes/all.js')
-
-const router = express.router()
-
-router.get('/browse', (res, req, next) => {
-    res.send(allClasses)
-});
-
