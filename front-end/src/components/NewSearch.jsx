169fc1496a70091f475def62a167e448383dc72f
Verse <verseow@gmail.com>
2022-04-04 16:59:29 -0400

    results on focus

--- front-end/src/components/NewSearch.jsx
+++ front-end/src/components/NewSearch.jsx
@@ -1,33 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import algoliasearch from 'algoliasearch';
 import { InstantSearch, SearchBox, Hits } from 'react-instantsearch-dom';
 
-const searchClient = algoliasearch(process.env.NEXT_PUBLIC_ALG_ID, process.env.NEXT_PUBLIC_ALG_KEY);
-// console.log(id);
-// console.log(key);
-// const searchClient = algoliasearch(
-//   'latency',
-//   '6be0576ff61c053d5f9a3225e2a90f76'
-// );
+const searchClient = algoliasearch(
+  process.env.NEXT_PUBLIC_ALG_ID,
+  process.env.NEXT_PUBLIC_ALG_KEY
+);
 const Hit = ({ hit }) => {
   return (
     <div>
-      <div>
-        {hit.name}
-      </div>
+      <div>{hit.name}</div>
     </div>
   );
 };
 
 const NewSearch = () => {
+  const [showHits, setShowHits] = useState(false);
+
   return (
     <InstantSearch
       searchClient={searchClient}
       indexName="classes"
       appId=""
       apiKey="">
-      <SearchBox translations={{ placeholder: 'Search for a class' }} />
-      <Hits hitComponent={Hit} />
+      <SearchBox
+        translations={{ placeholder: 'Search for a class' }}
+        onFocus={() => setShowHits(true)}
+        onBlur={() => setShowHits(false)}
+      />
+      {showHits ? <Hits hitComponent={Hit} /> : null}
     </InstantSearch>
   );
 };
