3102df317b831a095b84d21e8c445669afd11dd0
Verse <verseow@gmail.com>
2022-04-05 06:48:32 -0400

    connected frontend to notifications api

--- front-end/src/components/Notification.jsx
+++ front-end/src/components/Notification.jsx
@@ -1,19 +1,19 @@
 import {FaTimes} from 'react-icons/fa'
-const Notification = ({notif}) => {
+const Notification = ({ notification }) => {
     return(
       <div className="flex items-center justify-center grid grids-cols-1 mt-6">
       
          <div className = "bg-white p-3 rounded-2xl drop-shadow-xl hover:bg-white/75">
            <h1>
              <div className="flex gap-8">
-               {notif.title}
+               {notification.title}
                <button className="hover:cursor-pointer">
                  <FaTimes />
                </button> 
              </div>
            </h1>
            <p>
-             {notif.date}
+             {notification.date}
            </p>
          </div>
        
--- front-end/src/pages/notifications.js
+++ front-end/src/pages/notifications.js
@@ -1,50 +1,25 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { useRouter } from 'next/router';
-import Notification  from '../components/Notification';
+import Notification from '../components/Notification';
+import axios from 'axios';
 
 
-const Notifications = () => {
-  const [notifications, setNotifs] = useState([
-    {
-      id: 1,
-      title: 'Jordan liked your note!',
-      date: '4 days ago',
-    },
-    {
-      id: 2,
-      title: 'You added the class "Introduction to Computer Science"!',
-      date: '5 days ago',
-    },
-    {
-      id: 3,
-      title: 'Chris commented on your note!',
-      date: '1 week ago',
-    },
-    {
-      id: 4,
-      title: 'Your note was removed by an admin!',
-      date: '2 weeks ago',
-    },
-    {
-      id: 5,
-      title: 'Your comment was upvoted!',
-      date: '2 weeks ago',
-    },
-    {
-      id: 6,
-      title: 'You reached 200 total upvotes! Congratulations!',
-      date: '3 weeks ago',
-    },
-  ]  )
+const Notifications = (notifications) => {
   return (
     <>
-      {notifications.map((notif) => (
-        <Notification key={notif.id } notif={notif} />
+      {notifications.notifications.map((notification) => (
+        <Notification key={notification.id} notification={notification} />
       ))}
     </>
   );
-}
+};
 
-//Notifications.propTypes = {};
+Notifications.propTypes = {};
 export default Notifications;
+
+export const getStaticProps = async () => {
+  const res = await axios.get('http://localhost:3001/api/notifications/');
+  const notifications = await res.data;
+  return { props: { notifications } };
+};
