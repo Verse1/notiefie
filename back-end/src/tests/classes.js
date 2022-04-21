4f561ebfbe5e8444ef6f20df5aa8cd43e8aa22c2
Verse <verseow@gmail.com>
2022-04-21 13:22:25 -0400

    fixed all class tests

--- back-end/src/tests/classes.js
+++ back-end/src/tests/classes.js
@@ -58,39 +58,39 @@ describe('Classes API', () => {
       });
   });
 
-  // it('should return class by id', (done) => {
-  //   chai
-  //     .request(server)
-  //     .get('/api/classes/' + classs)
-  //     .end((err, res) => {
-  //       assert.equal(res.status, 200);
-  //       assert.property(res.body, 'id');
-  //       assert.property(res.body, 'name');
-  //       assert.property(res.body, 'classCode');
-  //       assert.property(res.body, 'university');
-  //       assert.property(res.body, 'createdAt');
-  //       done();
-  //     });
-  // });
+  it('should return class by id', (done) => {
+    chai
+      .request(server)
+      .get('/api/classes/' + classs)
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.property(res.body, '_id');
+        assert.property(res.body, 'className');
+        assert.property(res.body, 'classCode');
+        assert.property(res.body, 'university');
+        assert.property(res.body, 'createdAt');
+        done();
+      });
+  });
 
-  // it('should delete class by id', (done) => {
-  //   chai
-  //     .request(server)
-  //     .delete('/api/classes/621522b8-22b7-4634-befc-0a5785f53d3d')
-  //     .end((err, res) => {
-  //       assert.equal(res.status, 200);
-  //       assert.equal(res.text, 'Class deleted');
-  //       done();
-  //     });
-  // });
-  // it('not find deleted class', (done) => {
-  //   chai
-  //     .request(server)
-  //     .get('/api/classes/621522b8-22b7-4634-befc-0a5785f53d3d')
-  //     .end((err, res) => {
-  //       assert.equal(res.status, 404);
-  //       assert.equal(res.text, 'Class not found');
-  //       done();
-  //     });
-  // });
+  it('should delete class by id', (done) => {
+    chai
+      .request(server)
+      .delete('/api/classes/'+classs)
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.equal(res.text, 'Class deleted');
+        done();
+      });
+  });
+  it('should not find deleted class', (done) => {
+    chai
+      .request(server)
+      .get('/api/classes/'+classs)
+      .end((err, res) => {
+        assert.equal(res.status, 404);
+        assert.equal(res.text, 'Class not found');
+        done();
+      });
+  });
 });
