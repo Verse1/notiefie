2d08055fe5525bd4edfb4049779bad4cfe774e70
Verse <verseow@gmail.com>
2022-04-23 11:33:52 -0400

    fixed login page

--- front-end/src/pages/login.js
+++ front-end/src/pages/login.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 import { useAuth0 } from "@auth0/auth0-react";
 import axios from 'axios';
 
-export default function login() {
+export default function Login() {
 
   const { loginWithRedirect } = useAuth0();
 
@@ -11,7 +11,7 @@ export default function login() {
     <div className="grid h-[88vh] place-items-center">
       <div className="bg inline-block rounded-3xl bg-white p-7">
         <h1 className="mb-7 text-center text-2xl">Login to your account</h1>
-          <button onClick={() => loginWithRedirect()}>Log In</button>
+          <button onClick={loginWithRedirect}>Log In</button>
       </div>
     </div>
   );
