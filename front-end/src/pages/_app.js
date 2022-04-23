d8449a7599f9de0b89893040d31788bea8ee3fba
isabelleoktay <60681704+isabelleoktay@users.noreply.github.com>
2022-04-23 12:32:01 -0400

    fixed auth

--- front-end/src/pages/_app.js
+++ front-end/src/pages/_app.js
@@ -9,9 +9,9 @@ import { Auth0Provider } from "@auth0/auth0-react";
 function MyApp({ Component, pageProps }) {
   return (
     <Auth0Provider 
-      domain={process.env.AUTH0_ISSUER_BASE_URL}
-      clientId={process.env.AUTH0_CLIENT_ID}
-      redirectUri={process.env.AUTH0_BASE_URL}>
+      domain={'dev-z0la8at9.us.auth0.com'}
+      clientId={'zFBQbnVXZGYurCRU0FatAyY3CEoeMWjP'}
+      redirectUri={'http://localhost:3000/'}>
       <Head>
         <title>Notiefi</title>
         <link rel="icon" href="/favicon.ico" />
